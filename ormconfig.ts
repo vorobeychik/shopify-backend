e52612919eefca9c5d39bd51766a9aace43ba055
vorobeychik <vorob.edu@gmail.com>
2024-08-02 13:34:21 +0300

    initial commit

---
+++ ormconfig.ts
@@ -0,0 +1,11 @@
+module.exports = {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  };
+  
